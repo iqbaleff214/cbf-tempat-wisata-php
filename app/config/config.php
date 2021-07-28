06e3892af93c6087bb505c6e2675fff04dc52d58
M. Iqbal Effendi <iqbaleff214@gmail.com>
2021-07-28 16:56:06 +0800

    Add readme

--- app/config/config.php
+++ app/config/config.php
@@ -1,14 +1,19 @@
 <?php 
 
+# Base url seperti CI 3
 define('BASEURL', 'http://localhost/latihan/ta-andin/');
+
+# Nama aplikasi atau sistemnya
 define('APPNAME', 'REKOMENDASI WISATA');
 
+# Konfigurasi database
 define('HOSTNAME', 'localhost');
 define('USERNAME', 'admin');
 define('PASSWORD', 'admin');
 define('DATABASE', 'ta_andin');
 
 // Mengatur mode halaman
+# tidak terpakai
 define('DARKMODE', true);
 
 // Daftar halaman yang tersedia
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Sistem Rekomendasi
+
+Pastikan database sudah sesuai dengan yang ada di project.
+
+Untuk konfigurasi cukup cari file dengan nama `config.php` di `app/config/config.php`. Kemudian Anda cukup menyesuaikan nilai untuk baris 3-13, yaitu:
+
+- BASEURL
+- APPNAME
+- HOSTNAME
+- USERNAME
+- PASSWORD
+- DATABASE
+
+Selain dari itu, tidak perlu diubah.
